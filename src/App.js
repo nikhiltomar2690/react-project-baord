9f8b832c50c7ee4eb6d3b99f8299852f802abd79
Nikhil Tomar <nikhiltomar2690@gmail.com>
2023-11-10 15:20:43 +0530

    readme-update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import "./App.css";
-// import Todolist from "./components/Todolist";
 import TodoBoard from "./components/TodoBoard";
 const App = () => {
   return (
     <>
-      {/* <Todolist /> */}
       <TodoBoard />
     </>
   );
